d4e1a40e394e906ee61049a988d71b352cab96fd
Brian K. Jones <bkjones@gmail.com>
2009-12-17 05:30:18 +0800

    If no config arg is supplied, exit with an error. Use multiprocessing to spawn Application() instance for each port in config['tornado']['ports'] Still working out process management issues: namely, handling kill signals without orphaning children.

--- project.py
+++ project.py
@@ -19,6 +19,9 @@ __date__    = "2009-11-10"
 __version__ = 0.1
 
 
+import multiprocessing
+import logging
+import yaml
 import optparse
 import os
 import sys
@@ -52,9 +55,30 @@ class Application(tornado.web.Application):
         
         tornado.web.Application.__init__(self, handlers, **settings)
 
+def runapp(runport):
+    try:
+        http_server = tornado.httpserver.HTTPServer(Application(), no_keep_alive = False)
+        http_server.listen(runport)
+        tornado.ioloop.IOLoop.instance().start()
+    except Exception as out:
+        print out
+        raise
+    else:
+        print "Kickoff on port %d OK!!!!" % runport
+
+children = []
+def handle_sigterm(sig, frame): 
+    for child in children:
+        if child.is_alive():
+            print "terminating child: ", child.name
+            child.terminate()
+    print "Terminating self..." 
+    sys.exit(0)
+    
+
 if __name__ == "__main__":
 
-    usage = "usage: %prog [options]"
+    usage = "usage: %prog -c <configfile> [options]"
     version_string = "%%prog %s" % __version__
     description = "Project Name"
     
@@ -63,15 +87,39 @@ if __name__ == "__main__":
                          version=version_string,
                          description=description)
  
-    parser.add_option("-c", "--console",
-                        action="store_true", dest="console", default=False,
-                        help="Run interactively in console for debugging purposes")                                                                                                                                 
- 
+    parser.add_option("-c", "--config",
+                        action="store", dest="config",
+                        help="Specify the configuration file for use")            
+                               
+    parser.add_option("-f", "--foreground",
+                        action="store_true", dest="foreground", default=False,
+                        help="Run interactively in console for debugging purposes")                                                                                                                                             
+
+    parser.add_option("-p", "--port",
+                        action="store", dest="port", help="Specify the port to use")   
+
     # Parse our options and arguments                                                                        
     options, args = parser.parse_args()
     
+    if options.config is None:
+        print "Please provide a configuration file"
+        print usage
+        sys.exit(1)
+
+    # get running mode (i.e. 'stage' or 'production' from config filename option
+    mode = options.config.split('/')[-1].split('.')[0]
+
+    # try to load the config file. 
+    try:
+        stream = file(options.config, 'r')
+        config = yaml.load(stream)
+        stream.close()
+    except IOError:
+        sys.stderr.write('Invalid or missing configuration file \"%s\"' % options.config)
+        sys.exit(1)
+
     # Fork our process to detach if not told to stay in foreground
-    if not options.console:
+    if not options.foreground:
         try:
             pid = os.fork()
             if pid > 0:
@@ -87,12 +135,12 @@ if __name__ == "__main__":
                 # exit from second parent, print eventual PID before
                 print 'project.py daemon has started - PID # %d.' % pid
                 sys.exit(0) 
-        except OSError, e: 
+        except OSError, e:
             sys.stderr.write("Could not fork: %d (%s)\n" % (e.errno, e.strerror))
             sys.exit(1)
         
         # Detach from parent environment
-        os.chdir('~/Source/tornado-project-stub/') 
+        os.chdir('/') 
         os.setsid()
         os.umask(0) 
  
@@ -104,10 +152,19 @@ if __name__ == "__main__":
         sys.stderr = open('/dev/null', 'a')
 
     # Load the locales
-    tornado.locale.load_translations(
-        os.path.join(os.path.dirname(__file__), "translations"))
-    
+#    tornado.locale.load_translations(
+#        os.path.join(os.path.dirname(__file__), "translations"))
+#    signal.signal(signal.SIGTERM, handle_sigterm) 
     # Kick of the HTTP Server and Application                        
-    http_server = tornado.httpserver.HTTPServer(Application(), no_keep_alive = False)
-    http_server.listen(8080)
-    tornado.ioloop.IOLoop.instance().start()
+    if options.port is not None:
+        print "Kicking off tornado using options.port: ", options.port
+        http_server = tornado.httpserver.HTTPServer(Application(), no_keep_alive = False)
+        http_server.listen(int(options.port))
+        tornado.ioloop.IOLoop.instance().start()
+    else:
+        print "options.port is None, kicking off using config ports"
+        for p in config['tornado']['ports']:
+            proc = multiprocessing.Process(target=runapp, args=(p,))
+            proc.start()
+            children.append(proc)
+#

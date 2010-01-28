487e433bdcf0415375361b0f82e7c684ed98c996
gmr <gavinmroy@gmail.com>
2010-01-27 19:45:20 -0500

    More cleanup going from Tornado Project Stub to Tinman

--- tinman/apps.py
+++ tinman/apps.py
@@ -9,10 +9,11 @@ __email__   = "gavinmroy@gmail.com"
 __date__    = "2009-11-10"
 __version__ = 0.1
 
-import project.data
-import project.handler
-        
-class Home(project.handler.RequestHandler):
+import tinman.data as data
+import tinman.handler as handler
+
+class Home(handler.RequestHandler):
 
     def get(self):
-        self.render('templates/apps/home.html', title='Hello, World!');
+
+        self.render('templates/apps/home.html');

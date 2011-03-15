86e0a31ed60cc535f90a298330a8a381008062bf
Gavin M. Roy <gmr@myyearbook.com>
2011-03-15 00:59:35 -0400

    Make the whitelist accept an optional argument of str or list of whitelisted netblocks. Add unit tests for whitelist. PEP8 cleanup files.

--- test/example.py
+++ test/example.py
@@ -1,11 +1,13 @@
 from tinman.utils import log_method_call
 from tornado.web import RequestHandler
 
+
 class Home(RequestHandler):
 
     @log_method_call
     def get(self):
         self.write("Hello, World!")
+
 
 class Catchall(RequestHandler):
 
--- tinman/whitelist.py
+++ tinman/whitelist.py
@@ -9,9 +9,9 @@ __version__ = 0.1
 from functools import wraps
 from ipaddr import IPv4Network, IPv4Address
 from tornado.web import HTTPError
+from types import FunctionType
 
-
-def whitelisted(method):
+def whitelisted(argument=None):
     """
     Decorates a method requiring that the requesting IP address is whitelisted
 
@@ -21,27 +21,66 @@ def whitelisted(method):
     Examples:
         ['10.0.0.0/8','192.168.1.0/24', '1.2.3.4/32']
     """
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-
-        # Validate we have a configured whitelist
-        if 'whitelist' not in self.application.settings:
-            raise ValueError("whitelist is not found in Application.settings")
+    def is_whitelisted(remote_ip, whitelist):
 
         # Convert the ip into a long int version of the ip address
-        user_ip = IPv4Address(self.request.remote_ip)
+        user_ip = IPv4Address(remote_ip)
 
         # Loop through the ranges in the whitelist and check
-        for whitelist_ip in self.application.settings['whitelist']:
+        if any([user_ip in IPv4Network(entry) for entry in whitelist]):
+            return True
 
-            # Use IPv4 to return an ip address we can range check against
-            if user_ip in IPv4Network(whitelist_ip):
+        return False
+
+    # If the argument is a function then there were no parameters
+    if type(argument) is FunctionType:
+
+        def wrapper(self, *args, **kwargs):
+            """
+            Check the whitelist against our application.settings dictionary
+            whitelist key
+            """
+            # Validate we have a configured whitelist
+            if 'whitelist' not in self.application.settings:
+                raise ValueError("whitelist not found in Application.settings")
+
+            if is_whitelisted(self.request.remote_ip,
+                              self.application.settings['whitelist']):
 
                 # Call the original function, IP is whitelisted
-                return method(self, *args, **kwargs)
+                return argument(self, *args, **kwargs)
 
-        # The ip address was not in the whitelist
-        raise HTTPError(403)
+            # The ip address was not in the whitelist
+            raise HTTPError(403)
 
-    # Return the result of the require function
-    return wrapper
+        return wrapper
+
+    # They passed in string or list?
+    else:
+        # Convert a single ip address to a list
+        if isinstance(argument, str):
+            argument = [argument]
+
+        # Make sure it's a list
+        elif not isinstance(argument, list):
+            err = "whitelisted requires no parameters or a string or list"
+            raise ValueError(err)
+
+        def argument_wrapper(method):
+
+            def validate(self, *args, **kwargs):
+                """
+                Validate the ip address agross the list of ip addresses
+                passed in as a list
+                """
+                if is_whitelisted(self.request.remote_ip, argument):
+
+                    # Call the original function, IP is whitelisted
+                    return method(self, *args, **kwargs)
+
+                # The ip address was not in the whitelist
+                raise HTTPError(403)
+
+            return validate
+
+        return argument_wrapper

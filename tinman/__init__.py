7e97ddd43b8b388091e62f29d8a31875d8637d71
Gavin M. Roy <gmr@myyearbook.com>
2011-07-02 22:58:33 -0400

    Fix a small release bug

--- tinman/__init__.py
+++ tinman/__init__.py
@@ -5,7 +5,7 @@ Core Tinman imports
 __author__ = 'Gavin M. Roy'
 __email__ = '<gmr@myyearbook.com>'
 __since__ = '2011-03-14'
-__version__ = "0.2.5
+__version__ = "0.2.5"
 
 __all__ = ['tinman.application',
            'tinman.cache',

470a62125e5cd97baa72d5958b6e672ecaa70e3d
Gavin M. Roy <gmr@meetme.com>
2012-10-07 15:55:30 -0400

    Move the CouchDB template loader into a loaders package

---
+++ tinman/loaders/__init__.py
@@ -0,0 +1,4 @@
+"""
+Tornado Template Loaders
+
+"""
--- tinman/loaders/couchdb.py
+++ tinman/loaders/couchdb.py
@@ -1,22 +1,21 @@
 """
-External non-disk template loaders
+The CouchDB template loader allows for Tornado templates to be stored in CouchDB
+and retrieved on demand and supports all of the syntax of including and
+extending templates that you'd expect in any other template loader.
 
 """
-__author__ = 'Gavin M. Roy'
-__email__ = 'gmr@myyearbook.com'
-__since__ = '2011-08-25'
-
 import json
 import logging
 from tornado import escape
 from tornado import httpclient
 from tornado import template
 
+LOGGER = logging.getLogger(__name__)
+
 
 class CouchDBLoader(template.BaseLoader):
-    """
-    Extends the tornado.template.Loader allowing for templates to be loaded out
-    of CouchDB.
+    """Extends the tornado.template.Loader allowing for templates to be loaded
+    out of CouchDB.
 
     Templates in CouchDB should have have an _id matching the value of the name
     that is passed into load. _id's may have /'s in them. The template itself
@@ -27,12 +26,10 @@ class CouchDBLoader(template.BaseLoader):
         """Creates a template loader.
 
         """
-        template.BaseLoader.__init__(self, '/', **kwargs)
-        #super(CouchDBLoader, self).__init__(**kwargs)
-        self._logger = logging.getLogger('tinman.loader')
+        super(CouchDBLoader, self).__init__('/', **kwargs)
         self._base_url = base_url.rstrip('/')
-        self._logger.info('CouchDBLoader initialized with base URL of %s',
-                          self._base_url)
+        LOGGER.info('CouchDBLoader initialized with base URL of %s',
+                    self._base_url)
         self._http_client = httpclient.HTTPClient()
 
     def load(self, name, parent_path=None):
@@ -40,7 +37,6 @@ class CouchDBLoader(template.BaseLoader):
         """
         if name not in self.templates:
             self.templates[name] = self._create_template(name)
-
         return self.templates[name]
 
     def _create_template(self, name):
@@ -49,7 +45,7 @@ class CouchDBLoader(template.BaseLoader):
         url = '%s/%s' % (self._base_url, escape.url_escape(name))
 
         # Fetch the data from couchdb
-        self._logger.debug('Making HTTP GET request to %s', url)
+        LOGGER.debug('Making HTTP GET request to %s', url)
         response = self._http_client.fetch(url)
 
         # Load the data from the JSON string

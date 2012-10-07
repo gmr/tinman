481534dfc4ef5f461f6eefc4259e16855aefa9c4
Gavin M. Roy <gmr@meetme.com>
2012-10-07 16:00:51 -0400

    File removals

--- tinman/clients/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Tinman Client Sub-Package
-
-"""
-__author__ = 'Gavin M. Roy <gmr@myyearbook.com>'
-__since__ = "2011-07-02"
-
-# We don't define shortcuts here because someone shouldn't have to install
-# brukva or pika ot run tinman

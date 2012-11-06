0c5e033214a5439345f3aa0f1a6bda88caf193be
blaurencin <blaurencin@bestofmedia.com>
2012-11-06 09:16:07 +0100

    * added a parameter in the wrapper and a runtime class, both help testing a require_basic_auth-wrapped handler.

--- tinman/auth/__init__.py
+++ tinman/auth/__init__.py
@@ -1,3 +1,9 @@
 from basic import require_basic_auth
 from digest import digest_auth
 from ldapauth import auth_user_ldap
+
+class AmITesting(object):
+    def __init__(self):
+        self.am_i = False
+
+AM_I_TESTING = AmITesting()

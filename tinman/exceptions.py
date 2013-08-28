18f1d9852a450d5792051c104de1b7c907b0c3d3
Gavin M. Roy <gmr@meetme.com>
2013-08-28 00:41:44 -0400

    New NoRoutesException

--- tinman/exceptions.py
+++ tinman/exceptions.py
@@ -6,3 +6,8 @@ Tinman Exceptions
 class ConfigurationException(Exception):
     def __repr__(self):
         return 'Configuration for %s is missing or invalid' % self.args[0]
+
+
+class NoRoutesException(Exception):
+    def __repr__(self):
+        return 'No routes could be configured'

69857db5f0479ce78d82c594c871060f9ff3ea58
Gavin M. Roy <gavinmroy@gmail.com>
2013-11-08 00:20:18 -0500

    Minor import differences

--- tinman/handlers/__init__.py
+++ tinman/handlers/__init__.py
@@ -2,13 +2,6 @@
 application development.
 
 """
-HEAD = 'HEAD'
-GET = 'GET'
-POST = 'POST'
-DELETE = 'DELETE'
-PATCH = 'PATCH'
-PUT = 'PUT'
-OPTIONS = 'OPTIONS'
-
+from tinman.handlers.base import HEAD, GET, POST, DELETE, PATCH, PUT, OPTIONS
 from tinman.handlers.base import RequestHandler
-from tinman.handlers.session import SessionRequestHandler
+from tinman.handlers.base import SessionRequestHandler
